e0a4df00217fe675ff0ec28c8ff4899f7e784e71
a.krapilskiy <zuzuka28kavaii@gmail.com>
2025-01-12 17:26:27 +0300

    draft for chart

--- deploy/infra/install_istio.sh
+++ deploy/infra/install_istio.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 # install istio into k8s
 
@@ -9,5 +9,23 @@ helm install istio-base istio/base -n istio-system --set defaultRevision=default
 
 helm install istiod istio/istiod -n istio-system --wait
 
-kubectl create namespace istio-ingress
-helm install istio-ingress istio/gateway -n istio-ingress --wait
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+    { kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.0/standard-install.yaml; }
+
+kubectl apply -f - <<EOF
+apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: gateway
+  namespace: istio-ingress
+spec:
+  gatewayClassName: istio
+  listeners:
+  - name: default
+    hostname: "*.example.com"
+    port: 80
+    protocol: HTTP
+    allowedRoutes:
+      namespaces:
+        from: All
+EOF
